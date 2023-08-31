4d81f51eda350486fea5d0779bd7894688f71019
Hanrabbit06 <s2218@e-mirim.hs.kr>
2023-08-31 15:12:24 +0900

    test

--- Today/src/gui/test.java
+++ Today/src/gui/test.java
@@ -1,5 +1,18 @@
 package gui;
 
-public class test {
+import java.awt.Frame;
 
+import javax.swing.JFrame;
+
+
+public class test extends JFrame{
+	public test() {
+		Frame startFrame =  new Frame();
+		startFrame.setTitle("오늘 뭐 먹지?");
+		startFrame.setBounds(100,100,300,300);
+		startFrame.setVisible(true);
+	}
+	public static void main(String args[]) {
+		test frame = new test();
+	}
 }
