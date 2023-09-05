925a991c90fc69910f4342fe28703d75a47a439e
Hanrabbit06 <s2218@e-mirim.hs.kr>
2023-09-05 10:07:02 +0900

    start 버튼

--- Today/src/gui/test.java
+++ Today/src/gui/test.java
@@ -1,5 +1,6 @@
 package gui;
 
+import java.awt.Container;
 import java.awt.Frame;
 
 import javax.swing.JFrame;
@@ -7,10 +8,7 @@ import javax.swing.JFrame;
 
 public class test extends JFrame{
 	public test() {
-		Frame startFrame =  new Frame();
-		startFrame.setTitle("오늘 뭐 먹지?");
-		startFrame.setBounds(100,100,300,300);
-		startFrame.setVisible(true);
+		
 	}
 	public static void main(String args[]) {
 		test frame = new test();
