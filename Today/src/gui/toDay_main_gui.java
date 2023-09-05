17fcdd77864e2a7588c7a28c309a2e5782a29ca8
Hanrabbit06 <s2218@e-mirim.hs.kr>
2023-09-05 10:10:04 +0900

    start 버튼 추가

---
+++ Today/src/gui/toDay_main_gui.java
@@ -0,0 +1,56 @@
+package gui;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class toDay_main_gui extends JFrame{
+	public startP startP = null;
+	public void startframe() {
+		getContentPane().add(startP);
+	}
+	
+	public static void main(String[] args) {
+		toDay_main_gui main = new toDay_main_gui();
+		
+		main.startP = new startP(null);
+		
+		main.add(main.startP);
+		
+		main.setResizable(false);
+		main.setVisible(true);
+		main.setPreferredSize(new Dimension(840,840/12*9));
+		main.setSize(840,840/12*9);
+		main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		main.setLocationRelativeTo(null);
+		main.setTitle("오늘 뭐 먹지?");
+	}
+
+}
+
+class startP extends JPanel{
+	private JButton btnStart;
+	private JButton  btnchange;
+	private toDay_main_gui main;
+	
+	ImageIcon startbtn1 = new ImageIcon("./Button_Image/start_button_ver.1.png");
+	ImageIcon startbtn1_1 = new ImageIcon("./Button_Image/start_button_check_ver.1.png");
+	
+	public startP(JPanel win) {
+		this.main=main;
+		setLayout(null);
+		
+		btnStart = new JButton(startbtn1);
+		btnStart.setRolloverIcon(startbtn1_1);
+		btnStart.setBorderPainted(false);
+		btnStart.setSize(130, 57);
+		btnStart.setLocation(350, 420);
+		btnStart.setContentAreaFilled(false);
+		add(btnStart);
+	}
+}
